f36407b6157326f2b8069cd10d0fa0f6ad30eddc
lam.lhd <lam.lhd@mor.com.vn>
2023-07-27 14:36:16 +0700

    first commit

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Category
+
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)} #field slug kế thừa từ category_name
+    list_display = ('category_name', 'slug')    # hiển thị các fieldS
+
+admin.site.register(Category, CategoryAdmin)
