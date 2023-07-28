fb7ab01497c2803ae0ccda065ec02ef28eed12e1
lam.lhd <lam.lhd@mor.com.vn>
2023-07-28 11:55:55 +0700

    10-Paginator & Search

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('categoty/<slug:category_slug>/', views.store, name='products_by_category'),
+    path('categoty/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search'),
 
 ]
