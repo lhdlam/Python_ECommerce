7ccf3c21cfa0824b92e14200fd622a5e49c10f22
lam.lhd <lam.lhd@mor.com.vn>
2023-08-02 15:13:38 +0700

    django-admin-security

--- store/views.py
+++ store/views.py
@@ -1,39 +1,41 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, ReviewRating, ProductGallery
+from category.models import Category
+from carts.models import CartItem
+from django.db.models import Q
 
 from carts.views import _cart_id
-from orders.models import OrderProduct
-from store.forms import ReviewForm
-from .models import Product, ReviewRating
-from category.models import Category
-from carts.models import Cart, CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
-from django.db.models import Q
-from django.contrib import messages, auth
+from .forms import ReviewForm
+from django.contrib import messages
+from orders.models import OrderProduct
 
 
-# Create your views here.
-def  store(request, category_slug=None):
-  categories = None
-  products = None
-  
-  if category_slug != None:
-    categories = get_object_or_404(Category, slug = category_slug)
-    products = Product.objects.filter(category=categories ,is_available=True)
-    product_count = products.count()
-    paginator = Paginator(products, 3)
-    page = request.GET.get('page')
-    paged_products = paginator.get_page(page)
-    product_count = products.count()
-  else:
-    products = Product.objects.all().filter(is_available=True).order_by('id')
-    paginator = Paginator(products, 3)
-    page = request.GET.get('page')
-    paged_products = paginator.get_page(page)
-    product_count = products.count()
-    # print(products[0].get_url())
-  context = {'products': paged_products, 'product_count': product_count}
-  return render(request, 'store/store.html', context)
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
 
 def product_detail(request, category_slug, product_slug):
     try:
@@ -54,16 +56,17 @@ def product_detail(request, category_slug, product_slug):
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
     # Get the product gallery
-    # product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
 
     context = {
         'single_product': single_product,
         'in_cart'       : in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
-        # 'product_gallery': product_gallery,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
+
 
 def search(request):
     if 'keyword' in request.GET:
@@ -76,7 +79,8 @@ def search(request):
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
-  
+
+
 def submit_review(request, product_id):
     url = request.META.get('HTTP_REFERER')
     if request.method == 'POST':
