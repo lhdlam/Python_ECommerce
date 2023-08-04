954dec60a552472bebca1e179e2f582b9b3caa6e
lhdlam <119838153+lhdlam@users.noreply.github.com>
2023-08-04 16:59:36 +0700

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -157,9 +157,9 @@ MESSAGE_TAGS = {
 
 # SMTP configuration
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 
